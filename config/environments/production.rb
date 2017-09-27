0171682ea2e0a9122bee5fa9416c7bc0689c5b37
yo-no <spencerblake91@gmail.com>
2017-09-27 15:22:16 -0500

    production protocolz

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,7 +34,8 @@ Rails.application.configure do
       access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
       secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
       s3_region: ENV.fetch('AWS_REGION')
-    }
+    },
+    :s3_protocol => :https
   }
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
