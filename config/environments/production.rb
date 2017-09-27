8e9728f8fc2a3784b42702310f71a16fb5a32c14
yo-no <spencerblake91@gmail.com>
2017-09-27 16:51:11 -0500

    workaround

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,8 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
     storage: :s3,
-    s3_protocol: :https, 
+    s3_protocol: :https,
+    s3_host_name: "s3-#{ENV['AWS_REGION']}.amazonaws.com",
     s3_credentials: {
       bucket: ENV['S3_BUCKET_NAME'],
       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
