2cb2a4ae50889112a40121c2e979b128a3ef30f1
yo-no <spencerblake91@gmail.com>
2017-09-27 15:16:23 -0500

    werk

--- app/models/card.rb
+++ app/models/card.rb
@@ -4,8 +4,7 @@ class Card < ApplicationRecord
   # has_many :images, as: :imageable_type
 
   has_attached_file :image, 
-    styles: {large: "357x500>", medium: "214x300>", small: "143x200"},
-    s3_protocol: :https
+  styles: {large: "357x500>", medium: "214x300>", small: "143x200"}
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
   def medium_image_url
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,7 @@ class User < ApplicationRecord
 
   has_attached_file :avatar,
    styles: { large: "400x400#", big:"300x300#", medium: "200x200#", thumb: "100x100#" },
-   default_url: "/images/:style/missing.png",
-   s3_protocol: :https
+   default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
   def initialize_deck
